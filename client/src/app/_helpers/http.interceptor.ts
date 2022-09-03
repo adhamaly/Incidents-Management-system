388d288fc4e83657b372a0755412c11b0e4ad32e
adhamaly <adhamaly126@gmail.com>
2022-09-04 01:56:14 +0200

    Implement AuthInterceptor

---
+++ client/src/app/_helpers/http.interceptor.ts
@@ -0,0 +1,28 @@
+import { HTTP_INTERCEPTORS, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
+
+import { StorageService } from '../_services/storage.service';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private storageService: StorageService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this.storageService.getToken()}`,
+      },
+    });
+
+    return next.handle(req);
+  }
+}
